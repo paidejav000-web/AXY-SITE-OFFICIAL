3e8068d4051b9035a98fde938884762872d2b5fe
paider <paidejav000@graftonschools.org>
2026-05-26 08:24:21 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,19 +1,22 @@
 window.addEventListener("DOMContentLoaded", () => {
 
     const popup = document.getElementById("axyPopupOverlay");
+    const closeBtn = document.getElementById("axyPopupClose");
+    const button = document.getElementById("axyPopupButton");
+
+    // if popup doesn't exist on page, do nothing
     if (!popup) return;
 
-    const close = document.getElementById("axyPopupClose");
-    const btn = document.getElementById("axyPopupButton");
-
+    // show popup
     popup.classList.add("show");
 
-    close?.addEventListener("click", () => {
-        popup.classList.remove("show");
-    });
+    // close actions
+    if (closeBtn) {
+        closeBtn.onclick = () => popup.classList.remove("show");
+    }
 
-    btn?.addEventListener("click", () => {
-        popup.classList.remove("show");
-    });
+    if (button) {
+        button.onclick = () => popup.classList.remove("show");
+    }
 
 });
